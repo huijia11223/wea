4be8ce5cdaff9c585689a43f43955e095678b90f
Tony <tong@email.com>
2017-12-27 21:23:40 +0800

    加入显示天气信息的功能.

---
+++ app/src/main/java/com/example/weather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.weather.gson;
+
+/**
+ * Created by Administrator on 2017/12/27 0027.
+ */
+
+public class AQI {
+    public AQICity city;
+    public  class AQICity{
+        public  String aqi;
+        public String pm25;
+    }
+}
